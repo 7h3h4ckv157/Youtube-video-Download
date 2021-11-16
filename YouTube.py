30f45c06a0ce5510c761930b173c0edd5b869623
7h3h4ckv157 <58929890+7h3h4ckv157@users.noreply.github.com>
2021-11-16 21:08:19 +0530

    Update YouTube.py

--- YouTube.py
+++ YouTube.py
@@ -1,4 +1,10 @@
+#!/usr/env/python3 
+# Author : 7h3h4ckv157
+# https://github.com/7h3h4ckv157
+# https://twitter.com/7h3h4ckv157
+
 import pytube; source = input("enter the link:")
 src = pytube.YouTube(source)
+
 hack=src.streams.first()
 hack.download()
